61b7155b5d29a66db836f39bfa911f0bca94fdc7
Vinicius <vinicius.vidal@engenharia.ufjf.br>
2024-07-11 15:24:07 -0300

    seeing points all around

--- src/cloud_filter.cpp
+++ src/cloud_filter.cpp
@@ -258,7 +258,7 @@ const bool CloudFilter::filterBoatPoints(const PointIn& p)
 
 const bool CloudFilter::filterRange(const PointIn& p) 
 {
-    return std::sqrt(p.x * p.x + p.y * p.y) > max_xy_range_ || p.x <= 0.1;
+    return std::sqrt(p.x * p.x + p.y * p.y) > max_xy_range_;
 }
 
 const bool CloudFilter::filterIntensity(const PointIn& p) 
